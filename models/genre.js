006d66e48da30366b06758b9ba3b5db5dfea1f04
edzaniBruce51 <mutemulaeb51@gmail.com>
2025-05-16 13:49:35 +0200

    added the Genre model with its virtual Schemas

--- models/genre.js
+++ models/genre.js
@@ -4,7 +4,6 @@ const Schema = mongoose.Schema;
 
 const GenreSchema = new Schema({
     name: { type: String, required: true, minLength: 3, maxLength: 100 },
-    book: { type: Schema.Types.ObjectId, ref: "Book", required: true },
 });
 
 // Virtual for Genre's URL
