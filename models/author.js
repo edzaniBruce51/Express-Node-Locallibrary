fa5cab3718d773ccc2988a0cc312923d7a6aa3e4
edzaniBruce51 <mutemulaeb51@gmail.com>
2025-05-19 14:32:14 +0200

    updated dates on author model and created virtual properties for luxon formatting on author

--- models/author.js
+++ models/author.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -26,6 +27,17 @@ AuthorSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/author/${this._id}`;
 });
+
+//fromJSDate() to import a JavaScript date string and toLocaleString() to output the date in DATE_MED format in English: Apr 10th, 2023.
+AuthorSchema.virtual("date_of_birth_formatted").get(function () {
+  return this.date_of_birth ? DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+AuthorSchema.virtual("date_of_death_formatted").get(function () {
+  return this.date_of_death ? DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED) : '';
+});
+
+
 
 // Export model
 module.exports = mongoose.model("Author", AuthorSchema);
