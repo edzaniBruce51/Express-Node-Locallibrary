57e36ac83ca8de8d6eb0d61720b592ceca5554bd
edzaniBruce51 <mutemulaeb51@gmail.com>
2025-05-19 14:02:54 +0200

    Added a Luxon date format to due back date

--- models/bookinstance.js
+++ models/bookinstance.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const { DateTime } = require("luxon");
 
 const Schema = mongoose.Schema;
 
@@ -18,6 +19,11 @@ const BookInstanceSchema = new Schema({
 BookInstanceSchema.virtual("url").get(function () {
   // We don't use an arrow function as we'll need the this object
   return `/catalog/bookinstance/${this._id}`;
+});
+
+//fromJSDate() to import a JavaScript date string and toLocaleString() to output the date in DATE_MED format in English: Apr 10th, 2023.
+BookInstanceSchema.virtual("due_back_formatted").get(function () {
+  return DateTime.fromJSDate(this.due_back).toLocaleString(DateTime.DATE_MED);
 });
 
 // Export model
