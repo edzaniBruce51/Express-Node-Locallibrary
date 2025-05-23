dc9a2c9663557e46ed7a9cb742ef6132a62df2e8
edzaniBruce51 <mutemulaeb51@gmail.com>
2025-05-23 16:17:08 +0200

    Configurations for deployment on render

--- app.js
+++ app.js
@@ -13,11 +13,13 @@ var app = express();
 // Set up moongose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = "mongodb+srv://cooluser:EDZANi5*@cluster0.1z8dxiw.mongodb.net/local_library?retryWrites=true&w=majority&appName=Cluster0"
+
+// Use environment variable for MongoDB connection string
+const mongoDB = process.env.MONGODB_URI;
 
 main().catch((err) => console.log(err));
 async function main() {
-  await mongoose.connect(mongoDB);     // Connect to MongoDB
+  await mongoose.connect(mongoDB);
 }
 
 // view engine setup
